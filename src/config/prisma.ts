7f4adb6c8544abc60363d707bdfd623b7d957e53
fish-shy <hafiznugraha04@gmail.com>
2025-12-06 16:22:56 +0800

    update schema

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -2,8 +2,7 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-import { PrismaClient } from '../generated/prisma/client';
-
+import { PrismaClient } from '@prisma/client';
 dotenv.config();
 
 const connectionString = `${process.env.DATABASE_URL}`;
