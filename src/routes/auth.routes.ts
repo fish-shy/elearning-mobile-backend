61fc32f8614ff50e7d5b687e4d8bbe4078fc3ba0
fish-shy <hafiznugraha04@gmail.com>
2025-12-06 14:52:30 +0800

    grr

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,5 +7,5 @@ const router = Router();
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.get('/me', authenticateToken, authController.me);
-
+ 
 export default router;
